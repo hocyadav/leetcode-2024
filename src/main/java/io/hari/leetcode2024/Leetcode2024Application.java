5bbed60a522389d8c26838e396b032642788144c
Hariom Yadav <hoc.yadav@gmail.com>
2024-09-29 06:59:52 +0000

    Initial commit

---
+++ src/main/java/io/hari/leetcode2024/Leetcode2024Application.java
@@ -0,0 +1,13 @@
+package io.hari.leetcode2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Leetcode2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Leetcode2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/hari/leetcode2024/Leetcode2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hari.leetcode2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Leetcode2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
